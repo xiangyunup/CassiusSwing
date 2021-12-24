11d636c75a64bf27843ee287560ad89926cf75c0
lixiangyun <lixiangyun@biruiqi.com>
2021-12-24 10:34:59 +0800

    sql

---
+++ sql/nn_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `nn_user` (`id`, `avatar`, `gender`, `nike_name`, `user_name`, `pass_word`, `secret_key`, `is_valid`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (1, '02.png', 'nan', '开发者', 'root', '123456', NULL, 1, NULL, '2021-12-21 12:03:47', NULL, '2021-12-24 10:32:57');
